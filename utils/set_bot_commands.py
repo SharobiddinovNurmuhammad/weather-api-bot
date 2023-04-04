e17665903f8ea4fe8c9604221ebd0b6fb7637222
Your Name <sharobiddinovnurmuhammad23@gmail.com>
2023-04-04 23:27:51 +0500

    menu

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -7,6 +7,6 @@ async def set_default_commands(dp):
             types.BotCommand("start", "Botni ishga tushurish"),
             types.BotCommand("help", "Yordam"),
             types.BotCommand("search", "Qidiruv"),
-            types.BotCommand("menu", "Bosh menu")
+            types.BotCommand("menu", "Bosh menu"),
         ]
     )
