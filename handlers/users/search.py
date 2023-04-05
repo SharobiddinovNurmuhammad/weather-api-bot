74ea6a0c73575da2f3de7cf496252e4e4cd34853
Your Name <sharobiddinovnurmuhammad23@gmail.com>
2023-04-05 11:14:08 +0500

    search

--- handlers/users/search.py
+++ handlers/users/search.py
@@ -1,8 +1,10 @@
+import asyncio
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 from utils import weathers
-from loader import dp
+from loader import dp, bot
 
 @dp.message_handler(Command('search')) # /qidiruv
 async def search_bot(message: types.Message, state: FSMContext):
@@ -11,9 +13,14 @@ async def search_bot(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state='searchstate')
 async def get_search(message: types.Message, state: FSMContext):
+    search_icon = await bot.send_message(chat_id=message.from_user.id, text='🔍')
     try:
+        await asyncio.sleep(2)
+        await search_icon.delete()
         msg = await weathers.weatherbot(message.text)
         await message.reply(msg)
     except:
+        await asyncio.sleep(2)
+        await search_icon.delete()
         await message.answer('Natija topilmadi! :)')
     await state.finish()
